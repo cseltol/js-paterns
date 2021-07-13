d069b9851c95964e458bf718ccfe5be7b8f6127f
cseltol <64264529+cseltol@users.noreply.github.com>
2021-07-13 08:37:10 +0300

    initial

--- behaviour/template.js
+++ behaviour/template.js
@@ -7,11 +7,11 @@ class Employee {
   responsibilities() {}
 
   work() {
-    return `${this.name} выполняет ${this.responsibilities()}`
+    return `${this.name} do ${this.responsibilities()}`
   }
 
   getPaid() {
-    return `${this.name} имеет ЗП ${this.salary}`
+    return `${this.name} salary is ${this.salary}`
   }
 }
 
@@ -21,7 +21,7 @@ class Developer extends Employee {
   }
 
   responsibilities() {
-    return 'процесс создания программ'
+    return 'create programms'
   }
 }
 
@@ -31,14 +31,14 @@ class Tester extends Employee {
   }
 
   responsibilities() {
-    return 'процесс тестирования'
+    return 'tests'
   }
 }
 
-const dev = new Developer('Владилен', 100000)
+const dev = new Developer('Daniel', 100000)
 console.log(dev.getPaid())
 console.log(dev.work())
 
-const tester = new Tester('Виктория', 90000)
+const tester = new Tester('Samuel', 90000)
 console.log(tester.getPaid())
 console.log(tester.work())
